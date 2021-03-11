e65dec54c5ea6726823afe82faec41f36fdd6c94
Murat Kudainetov <murat.kudainetov@gmail.com>
2021-03-11 12:47:55 +0300

    делаю блок под командой на основе блока attention. Нужен рефакторинг

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
 $(document).ready(function () {
+    if ($('.team__list')) {
+        function initMobileCarousel() {
+            var checkWidth = $(window).width();
+            var owlCarousel = $(".team__list");
+            if (checkWidth > 767) {
+                if (typeof owlCarousel.data('owl.carousel') != 'undefined') {
+                    owlCarousel.data('owl.carousel').destroy();
+                }
+                owlCarousel.removeClass('owl-carousel');
+            } else if (checkWidth < 768) {
+                owlCarousel.addClass('owl-carousel');
+                owlCarousel.owlCarousel({
+                    items: 1,
+                    autoplay: true,
+                    autoplayTimeout: 5000,
+                    dots: true,
+                    loop: true
+                });
+            }
+        }
 
+        initMobileCarousel();
+        $(window).resize(initMobileCarousel);
+    }
 });
